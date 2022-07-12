ef944cc320cf5b19dbcb6d829e28648271a76bfa
nadezhda <nadezhda.bessonova@gmail.com>
2022-07-12 16:26:47 +0500

    программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -9,3 +9,17 @@ Console.Write("Введите трёхзначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"Вторая цифра данного числа: {TheSecondDigit(num)}");*/
 
+
+//программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+int TheThirdDigit (int n)
+{
+    int number = n / 100 % 10;
+    return number;
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (TheThirdDigit(num) > 0) Console.WriteLine($"Третья цифра данного числа: {TheThirdDigit(num)}");
+    else Console.WriteLine("Третья цифра в данном числе отсутствует");
