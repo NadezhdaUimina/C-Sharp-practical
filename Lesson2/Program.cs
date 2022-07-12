04844789b7477501fe2abfbc20f989e9fa7d6e17
nadezhda <nadezhda.bessonova@gmail.com>
2022-07-12 15:57:47 +0500

    программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа

---
+++ Lesson2/Program.cs
@@ -0,0 +1,11 @@
+﻿/*// программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
+int TheSecondDigit (int n)
+{
+    return (n % 100 / 10);
+}
+
+Console.Write("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Вторая цифра данного числа: {TheSecondDigit(num)}");*/
+
